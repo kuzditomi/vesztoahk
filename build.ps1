5e76d07848a70806ddadb30b1d7cb78d61cac25a
kuzditomi <kuzditomi@gmail.com>
2022-09-05 18:32:09 +0200

    test build 3

--- build.ps1
+++ build.ps1
@@ -1,14 +1,16 @@
 $cwd = (Get-Item .).FullName;
+Write-Output ("$cwd\_autohotkey\;" + "$cwd\_autohotkey\Compiler") | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 
 function downloadAhk {
     Invoke-WebRequest "https://www.autohotkey.com/download/ahk.zip" -OutFile "$cwd\autohotkey.zip";
     Expand-Archive -Path "$cwd\autohotkey.zip" -DestinationPath "$cwd\_autohotkey\" -Force;
     Remove-Item -Path "$cwd\autohotkey.zip" -Force
-    Write-Output ("$cwd\_autohotkey\;" + "$cwd\_autohotkey\Compiler") | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
     Copy-Item -Path "$cwd\_autohotkey\Compiler\Unicode 64-bit.bin" -Destination "$cwd\_autohotkey\Compiler\AutoHotkeySC.bin"
 }
 
 function buildScripts {
+    Write-Output ("$cwd\_autohotkey\;" + "$cwd\_autohotkey\Compiler") | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+
     echo "gh path: $($env:GITHUB_PATH)"
     echo "scriptek: $cwd\scripts\*.ahk"
 
