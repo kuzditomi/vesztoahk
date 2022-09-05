9ae432ed08b396778c950ef22319dccd617c3155
kuzditomi <kuzditomi@gmail.com>
2022-09-05 18:55:52 +0200

    test build 6

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ function downloadAhk {
     Invoke-WebRequest "https://www.autohotkey.com/download/ahk.zip" -OutFile "$cwd\autohotkey.zip";
     Expand-Archive -Path "$cwd\autohotkey.zip" -DestinationPath "$cwd\_autohotkey\" -Force;
     
-    Copy-Item -Path "$cwd\_autohotkey\Compiler\Ahk2Exe.exen" -Destination "$cwd\ahk2exe.exe"
+    Copy-Item -Path "$cwd\_autohotkey\Compiler\Ahk2Exe.exe" -Destination "$cwd\ahk2exe.exe"
     Copy-Item -Path "$cwd\_autohotkey\Compiler\Unicode 64-bit.bin" -Destination "$cwd\AutoHotkeySC.bin"
 
     Remove-Item -Path "$cwd\autohotkey.zip" -Force
@@ -16,7 +16,7 @@ function buildScripts {
 
     Get-ChildItem -Path "$cwd\scripts\*.ahk" | ForEach-Object `
     {
-        $command = 'ahk2exe.exe /silent verbose '
+        $command = './ahk2exe.exe /silent verbose '
         $command += '/in "' + $_.fullname + '"'
                 
         $command += " | Write-Output"
