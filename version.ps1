88f397bfd8cc8ed8af42a4e0cb0271f34520b18d
kuzditomi <kuzditomi@gmail.com>
2022-09-13 20:04:38 +0200

    create release workflow

---
+++ version.ps1
@@ -0,0 +1,12 @@
+git --version
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
+
+$latestTagCommit = @(git rev-list --tags --max-count=1)
+$latestTag = @(git describe --tags $latestTagCommit)
+
+$latestVersion = [version]($latestTag.substring(1))
+$newTag = "v$($latestVersion.Major).$($latestVersion.Minor+1)"
+
+echo "new tag: $newTag"
+echo "NEW_VERSION=$newTag" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
