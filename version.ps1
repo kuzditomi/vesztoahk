295add5aba81546352a4a23b3bb49b6bea878ee3
kuzditomi <kuzditomi@gmail.com>
2022-09-13 20:01:49 +0200

    fix release

--- version.ps1
+++ version.ps1
@@ -3,9 +3,7 @@ git config user.name "GitHub Actions Bot"
 git config user.email "<>"
 
 $latestTagCommit = @(git rev-list --tags --max-count=1)
-$latestTagCommit
 $latestTag = @(git describe --tags $latestTagCommit)
-$latestTag
 
 $latestVersion = [version]($latestTag.substring(1))
 $newTag = "v$($latestVersion.Major).$($latestVersion.Minor+1)"
